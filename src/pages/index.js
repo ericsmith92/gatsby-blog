c36945ff2d13b7f8b7b1b9ecfe756295f5881715
ericsmith92 <ericsmth884@gmail.com>
2021-02-06 18:18:02 -0500

    finish basic intro to gatsby, added some styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components";
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) =>{
 console.log(data);
@@ -17,7 +27,9 @@ console.log(data);
           {
             data.allMarkdownRemark.edges.map(({node}) => (
               <div key={node.id}>
-                <span>{ node.frontmatter.title} - {node.frontmatter.date}</span>
+                <BlogLink to={node.fields.slug}>
+                  <BlogTitle>{ node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+                </BlogLink>
                 <p>{node.excerpt}</p>
               </div>  
             ))
@@ -28,7 +40,7 @@ console.log(data);
 
 export const query = graphql`
  query{
-  allMarkdownRemark {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
     edges {
       node {
         id
@@ -36,6 +48,9 @@ export const query = graphql`
           date
           description
           title
+        }
+        fields {
+            slug
         }
         html
         excerpt
